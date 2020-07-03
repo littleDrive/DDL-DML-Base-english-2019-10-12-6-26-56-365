90d600f350e2f7391b682d6779457d3a5ce9ccdf
littleDrive <2682155146@qq.com>
2020-07-03 13:43:11 +0800

    second commit

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+SHOW DATABASES;
 -- Enter a certain database
-
+USE database_name;
 -- Create a database
-
+CREATE DATABASE database_name;
 -- Create the database of the designated character set
-
+CREATE DATABASE database_name DEFAULT CHARACTER SET = 'utf8'
 -- Display the creation information fo the database
-
+SHOW CREATE DATABASE database_name;
 -- Revise the codes of the database
 
 -- Delete a database
-
+DROP DATABASE database_name;
 -- **Table level**
 -- Revise table name
-
+ALTER TABLE table_name REMANE new_table_name;
 -- Revise the field's data type
-
+ALTER TABLE table_name MODIFY COLUMN column_name new_type;
 -- Revise field name
-
+ALTER TABLE table_name CHANGE old_column_name new_column_name new_type;
 -- Add field
-
+ALTER TABLE table_name ADD COLUMN column_name type ;
 -- Delete field
-
+ALTER TABLE table_name DROP COLUMN column_name ;
 -- Revise the table's storage engine
-
+ALTER TABLE table_name ENGINE = engine_name;
 -- Delete the table's foreign key restriant
-
+ALTER TABLE table_name DROP FOREIGN KEY  foreign_name;
 -- Delete a table
+DROP TABLE table_name ;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+INSERT INTO student(id, name, age, sex) VALUES ("001", "Colin", 18, "male"), ("002", "Fmma", 20, "female");
 -- Revise record
-
+UPDATE student SET name="revise" WHERE id = "001";
 --  Delete record
-
+DELETE FROM student where id = "002";
 -- Search record
+SELECT * FROM student;
